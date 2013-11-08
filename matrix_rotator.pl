2094bdd930b942945f0618b59887a8d2260f5c4d
riddlemequick <riddlemequick@gmail.com>
2013-11-08 10:52:39 -0800

    A creative and easy way to rotate a 2d matrix of any size and shape (90 degree clockwise)
    1. swap x axis to y axis
    2. loop in reverse through new x axis

---
+++ matrix_rotator.pl
@@ -0,0 +1,27 @@
+#! usr/bin/perl -w
+use strict;
+ 
+# create an array of references, top left data being x=0, y=0 
+my @ref_matrix = ( [1,2,3,4] , 	   
+                   [5,6,7,8] );
+
+
+# show original data
+print "from:\n";
+for (@ref_matrix)
+{ for (@{$_}) { print "\t$_"; } print "\n"; }
+
+
+# create two nested loops that simply swap x axis to y axis (x = outer loop, y = inner loop)
+# outer loop will keep looping while first index of first array contains data
+print "to\n";
+while (defined ${$ref_matrix[0]}[0])
+{
+	# assign each reference to $y (in reverse) for each loop 
+	# inner loop will loop through original y axis in reverse
+	# each inner loop will pop out the first element of original x axis and shorten the array by 1 element
+	for my $y (reverse @ref_matrix)	  
+	{ print "\t". shift @{$y}; } 
+	print "\n";
+}
+<STDIN>;
